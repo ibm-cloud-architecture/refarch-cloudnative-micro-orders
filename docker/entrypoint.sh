99b8341b1846336ce1acbad33164d08bec9f04bf
Jeffrey Kwong <jkwong@Jeffreys-MacBook-Pro.local>
2017-05-31 13:35:12 -0400

    add heapsize parameter to java startup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,8 +2,12 @@
 
 set -e
 
+# find the java heap size as 50% of container memory using sysfs
+max_heap=`cat /sys/fs/cgroup/memory/memory.limit_in_bytes | xargs -I{} echo "({} / 1024 / 1024) / 2" | bc`
+export JAVA_OPTS="${JAVA_OPTS} -Xmx${max_heap}m"
+
 # Set basic java options
-export JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom"
+export JAVA_OPTS="${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom"
 
 # Load agent support if required
 source ./agents/newrelic.sh
