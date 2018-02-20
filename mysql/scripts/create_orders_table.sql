24b5af5f8b3f238f7b357e46c48a849437aff666
Hemankita Perabathini <hemankita.perabathini@ibm.com>
2018-02-20 17:24:36 -0600

    Changing the id to varchar

--- mysql/scripts/create_orders_table.sql
+++ mysql/scripts/create_orders_table.sql
@@ -1,5 +1,5 @@
 create table if not exists orders (
-    orderId bigint not null auto_increment primary key,
+    orderId varchar(64) not null primary key,
     itemId int not null,
     customerId varchar(64) not null,
     count int not null
