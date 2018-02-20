0b0e5de8b5e90ab860fbebba14ed0ee6259aed69
Hemankita Perabathini <hemankita.perabathini@ibm.com>
2018-02-20 17:31:12 -0600

    Adding response and fixing useSSL

--- src/main/java/application/rest/OrderService.java
+++ src/main/java/application/rest/OrderService.java
@@ -1,8 +1,10 @@
 package application.rest;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.UUID;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -10,9 +12,16 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 
 import com.google.gson.Gson;
+import com.ibm.websphere.security.openidconnect.PropagationHelper;
+import com.ibm.websphere.security.openidconnect.token.IdToken;
 
+import config.JwtConfig;
 import model.Order;
 import utils.OrderDAOImpl;
 
@@ -29,15 +38,18 @@ public class OrderService {
 	
 	@GET
 	 @Produces("application/json")
-	 public String getOrders() {
+	 public Response getOrders() {
 		
-		String orderDetails = null;
+		JwtConfig jwt = getJwt();
+		
+		//String orderDetails = null;
 		
 		try {
-         	final String customerId = getCustomerId();
+         	final String customerId = jwt.getUser_name();
         	if (customerId == null) {
         		// if no user passed in, this is a bad request
-        		return "Invalid Bearer Token: Missing customer ID";
+        		// return "Invalid Bearer Token: Missing customer ID";
+        		return Response.status(Response.Status.BAD_REQUEST).entity("Invalid Bearer Token: Missing customer ID").build();
         	}
         	
         	System.out.println("caller: " + customerId);
@@ -46,14 +58,17 @@ public class OrderService {
             
         	final List<Order> orders = ordersRepo.findByCustomerIdOrderByDateDesc(customerId);
         	
-        	Gson gson = new Gson();
-        	orderDetails = gson.toJson(orders);
-   		    return orderDetails;
+        	//Gson gson = new Gson();
+        	//orderDetails = gson.toJson(orders);
+   		    //return orderDetails;
+        	
+        	return Response.ok(orders).build();
             
         } catch (Exception e) {
             System.err.println(e.getMessage() +""+ e);
             // Include Http client later
-            return "Status not found";
+            // return "Status not found";
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
 	 
 	 }
@@ -61,15 +76,18 @@ public class OrderService {
 	@GET
 	 @Path("{id}")
 	 @Produces("application/json")
-	public String getById(@PathParam("id") String id) {
+	public Response getById(@PathParam("id") String id) {
+		
+		JwtConfig jwt = getJwt();
         
-		String orderDetails = null;
+		//String orderDetails = null;
 		
 		try {
-         	final String customerId = getCustomerId();
+         	final String customerId = jwt.getUser_name();
         	if (customerId == null) {
         		// if no user passed in, this is a bad request
-        		return "Invalid Bearer Token: Missing customer ID";
+        		// return "Invalid Bearer Token: Missing customer ID";
+        		return Response.status(Response.Status.BAD_REQUEST).entity("Invalid Bearer Token: Missing customer ID").build();
         	}
         	
         	System.out.println("caller: hello " + id);
@@ -91,49 +109,65 @@ public class OrderService {
     				
     		} 
         	
-        	Gson gson = new Gson();
-        	orderDetails = gson.toJson(findByOrderId);
-   		    return orderDetails;
+        	//Gson gson = new Gson();
+        	//orderDetails = gson.toJson(findByOrderId);
+   		    //return orderDetails;
+        	
+        	return Response.ok(findByOrderId).build();
             
         } catch (Exception e) {
             System.err.println(e.getMessage() +""+ e);
             // Include Http client later
-            return "Status not found";
+            // return "Status not found";
+            return Response.status(Response.Status.NOT_FOUND).build();
         }
 	}
 	
 	@POST
 	@Consumes("application/json")
 	@Produces("application/json")
-	public String create(Order payload) {
+	public Response create(Order payload, @Context UriInfo uriInfo) {
+		
+		JwtConfig jwt = getJwt();
+		
         try {
 			
-    		final String customerId = getCustomerId();
+    		final String customerId = jwt.getUser_name();
 			if (customerId == null) {
 				// if no user passed in, this is a bad request
-				return "Invalid Bearer Token: Missing customer ID";
+				//return "Invalid Bearer Token: Missing customer ID";
+				return Response.status(Response.Status.BAD_REQUEST).entity("Invalid Bearer Token: Missing customer ID").build();
 			}
 	         
 			payload.setDate(Calendar.getInstance().getTime());
 			payload.setCustomerId(customerId);
-    		
+			
+			String id = UUID.randomUUID().toString();
+			
+			payload.setId(id);
+			
 			System.out.println("New order: " + payload.toString());
 			
     		OrderDAOImpl ordersRepo = new OrderDAOImpl();
 			ordersRepo.putOrderDetails(payload);
 			
-			return payload.toString() + " created";
+			//return payload.toString() + " created";
+			
+            return Response.status(Response.Status.OK).entity(payload +" posted").build();
 			
         } catch (Exception ex) {
             System.err.println("Error creating order: " + ex);
-            return "Error creating order: " + ex.toString();
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Error creating order: " + ex.toString()).build();
         }
         
     }
 	
-	private String getCustomerId() {
-		//to be replaced by the customer using the security context
-		return "1";
+	private JwtConfig getJwt(){
+    	IdToken token = PropagationHelper.getIdToken();
+    	String claims = token.getAllClaimsAsJson();
+        Gson g = new Gson();
+        JwtConfig jwt = g.fromJson(claims, JwtConfig.class);
+    	return jwt;
     }
 
 }
