9d74fa1916cbfbde7c4eb7c6f46eed7d39b2b6c2
Hemankita Perabathini <hemankita.perabathini@ibm.com>
2018-04-10 17:17:10 -0500

    Adding date column

--- mysql/scripts/create_orders_table.sql
+++ mysql/scripts/create_orders_table.sql
@@ -1,7 +1,7 @@
 create table if not exists orders (
-    orderId varchar(64) not null primary key,
-    itemId int not null,
-    customerId varchar(64) not null,
-    count int not null
+   orderId varchar(64) not null primary key,
+   itemId int not null,
+   customerId varchar(64) not null,
+   count int not null,
+   date timestamp not null
 );
-
