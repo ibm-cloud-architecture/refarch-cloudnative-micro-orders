8e680edbdb19fcadfd2db76091d50871fd50340c
Hemankita Perabathini <hemankita.perabathini@ibm.com>
2018-05-24 16:59:02 -0500

    RabbitMQ health check

--- src/main/java/application/rest/HealthEndpoint.java
+++ src/main/java/application/rest/HealthEndpoint.java
@@ -1,11 +1,21 @@
 package application.rest;
 
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 import javax.enterprise.context.ApplicationScoped;
 
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.health.Health;
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.GetResponse;
 
 import utils.JDBCConnection;
 
@@ -13,6 +23,9 @@ import utils.JDBCConnection;
 @ApplicationScoped
 
 public class HealthEndpoint implements HealthCheck {
+	
+	private final static String QUEUE_NAME = "ordershealth";
+	String health = "orders_health_check";
 	
     public boolean isOrdersDbReady(){
 	   //Checking if Orders database is UP
@@ -31,15 +44,65 @@ public class HealthEndpoint implements HealthCheck {
 		
 		//Checking if RabbitMQ is UP
 
-		return true;
+		boolean msgStatus = sendMessage();
+
+		if(msgStatus == true){
+			return true;
+		}
+		return false;
 
 	}
+	
+	public boolean sendMessage(){
+		try
+		{
+			ConnectionFactory factory = new ConnectionFactory();
+			Config config = ConfigProvider.getConfig();
+			String rabbit_host = config.getValue("rabbit", String.class);
+			String sentMsg = null;
+			factory.setHost(rabbit_host);
+			Connection connection = factory.newConnection();
+			Channel channel = connection.createChannel();
+
+			channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+			channel.basicPublish("", QUEUE_NAME, null, health.getBytes());
+			System.out.println("Sent the message '" + health + "'");
+
+			boolean autoAck = true;
+			GetResponse response = channel.basicGet(QUEUE_NAME, autoAck);
+			if (response == null) {
+				// There are no messages to retrieve
+			}
+			else
+			{
+				byte[] body = response.getBody();
+				sentMsg = new String(body);
+			}
+
+			if(sentMsg.equals(health)){
+				return true;
+			}
+
+			channel.close();
+			connection.close();
+			return false;
+		}
+		catch(IOException e){
+			e.printStackTrace();
+			return false;
+		}
+		catch(TimeoutException e){
+			e.printStackTrace();
+			return false;
+		}
+		}
 	
     public boolean isAuthReady() {
 		
 		//Checking if Auth service is UP
 
-		return true;
+		return false;
 
 	}
     
@@ -47,7 +110,7 @@ public class HealthEndpoint implements HealthCheck {
 		
 		//Checking if Inventory service is UP
 
-		return true;
+		return false;
 
 	}
 
