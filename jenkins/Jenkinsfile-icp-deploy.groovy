2006b385a515eb4daa488e7ef4661712b736fb7a
Fabio Gomez <fabiogomez@us.ibm.com>
2018-12-18 15:58:24 -0600

    - New pipelines

---
+++ jenkins/Jenkinsfile-icp-deploy.groovy
@@ -0,0 +1,156 @@
+/*
+    To learn how to use this sample pipeline, follow the guide below and enter the
+    corresponding values for your environment and for this repository:
+    - https://github.com/ibm-cloud-architecture/refarch-cloudnative-devops-kubernetes
+*/
+
+// Environment
+def clusterURL = env.CLUSTER_URL
+def clusterAccountId = env.CLUSTER_ACCOUNT_ID
+def clusterCredentialId = env.CLUSTER_CREDENTIAL_ID ?: "cluster-credentials"
+
+// Pod Template
+def podLabel = "orders"
+def cloud = env.CLOUD ?: "kubernetes"
+def registryCredsID = env.REGISTRY_CREDENTIALS ?: "registry-credentials-id"
+def serviceAccount = env.SERVICE_ACCOUNT ?: "jenkins"
+
+// Pod Environment Variables
+def namespace = env.NAMESPACE ?: "default"
+def registry = env.REGISTRY ?: "docker.io"
+def imageName = env.IMAGE_NAME ?: "ibmcase/bluecompute-orders"
+def imageTag = env.IMAGE_TAG ?: "latest"
+def serviceLabels = env.SERVICE_LABELS ?: "app=orders,tier=backend" //,version=v1"
+def microServiceName = env.MICROSERVICE_NAME ?: "orders"
+def servicePort = env.MICROSERVICE_PORT ?: "8084"
+def managementPort = env.MANAGEMENT_PORT ?: "8094"
+
+// External Test Database Parameters
+// For username and passwords, set MYSQL_USER (as string parameter) and MYSQL_PASSWORD (as password parameter)
+//     - These variables get picked up by the Java application automatically
+//     - There were issues with Jenkins credentials plugin interfering with setting up the password directly
+
+def mariaDBHost = env.MYSQL_HOST
+def mariaDBPort = env.MYSQL_PORT ?: "3306"
+def mariaDBDatabase = env.MYSQL_DATABASE ?: "ordersdb"
+
+// HS256_KEY Secret
+def hs256Key = env.HS256_KEY
+
+/*
+  Optional Pod Environment Variables
+ */
+def helmHome = env.HELM_HOME ?: env.JENKINS_HOME + "/.helm"
+
+podTemplate(label: podLabel, cloud: cloud, serviceAccount: serviceAccount, namespace: namespace, envVars: [
+        envVar(key: 'CLUSTER_URL', value: clusterURL),
+        envVar(key: 'CLUSTER_ACCOUNT_ID', value: clusterAccountId),
+        envVar(key: 'NAMESPACE', value: namespace),
+        envVar(key: 'REGISTRY', value: registry),
+        envVar(key: 'IMAGE_NAME', value: imageName),
+        envVar(key: 'IMAGE_TAG', value: imageTag),
+        envVar(key: 'SERVICE_LABELS', value: serviceLabels),
+        envVar(key: 'MICROSERVICE_NAME', value: microServiceName),
+        envVar(key: 'MICROSERVICE_PORT', value: servicePort),
+        envVar(key: 'MANAGEMENT_PORT', value: managementPort),
+        envVar(key: 'MYSQL_HOST', value: mariaDBHost),
+        envVar(key: 'MYSQL_PORT', value: mariaDBPort),
+        envVar(key: 'MYSQL_DATABASE', value: mariaDBDatabase),
+        envVar(key: 'HS256_KEY', value: hs256Key),
+        envVar(key: 'HELM_HOME', value: helmHome)
+    ],
+    containers: [
+        containerTemplate(name: 'kubernetes', image: 'ibmcase/jenkins-slave-utils:latest', ttyEnabled: true, command: 'cat')
+  ]) {
+
+    node(podLabel) {
+        checkout scm
+
+        // Kubernetes
+        container(name:'kubernetes', shell:'/bin/bash') {
+            stage('Initialize CLIs') {
+                withCredentials([usernamePassword(credentialsId: clusterCredentialId,
+                                               passwordVariable: 'CLUSTER_PASSWORD',
+                                               usernameVariable: 'CLUSTER_USERNAME')]) {
+                    sh """
+                    echo "Initializing Helm ..."
+                    export HELM_HOME=${HELM_HOME}
+                    helm init -c
+
+                    echo "Login with cloudctl ..."
+                    cloudctl login -a ${CLUSTER_URL} -u ${CLUSTER_USERNAME}  -p "${CLUSTER_PASSWORD}" -c ${CLUSTER_ACCOUNT_ID} -n ${NAMESPACE} --skip-ssl-validation
+                    """
+                }
+            }
+            stage('Kubernetes - Deploy new Docker Image') {
+                sh """
+                #!/bin/bash
+
+                # Get image
+                if [ "${REGISTRY}" == "docker.io" ]; then
+                    IMAGE=${IMAGE_NAME}
+                else
+                    IMAGE=${REGISTRY}/${NAMESPACE}/${IMAGE_NAME}
+                fi
+
+                # Helm Parameters
+                if [ "${DEPLOY_NEW_VERSION}" == "true" ]; then
+                    NAME="${MICROSERVICE_NAME}-v${IMAGE_TAG}"
+                    VERSION_LABEL="--set labels.version=v${IMAGE_TAG}"
+                else
+                    NAME="${MICROSERVICE_NAME}"
+                fi
+
+                echo "Installing chart/${MICROSERVICE_NAME} chart with name \${NAME} and waiting for pods to be ready"
+
+                set +x
+                helm upgrade --install \${NAME} --namespace ${NAMESPACE} \${VERSION_LABEL} \
+                    --set fullnameOverride=\${NAME} \
+                    --set image.repository=\${IMAGE} \
+                    --set image.tag=${IMAGE_TAG} \
+                    --set service.externalPort=${MICROSERVICE_PORT} \
+                    --set mariadb.host=${MYSQL_HOST} \
+                    --set mariadb.port=${MYSQL_PORT} \
+                    --set mariadb.database=${MYSQL_DATABASE} \
+                    --set mariadb.user=${MYSQL_USER} \
+                    --set mariadb.password=${MYSQL_PASSWORD} \
+                    --set hs256key.secret="${HS256_KEY}" \
+                    chart/${MICROSERVICE_NAME} --wait --tls
+                set -x
+                """
+            }
+            stage('Kubernetes - Test') {
+                sh """
+                #!/bin/bash
+
+                # Get deployment
+                if [ "${DEPLOY_NEW_VERSION}" == "true" ]; then
+                    QUERY_LABELS="${SERVICE_LABELS},version=v${IMAGE_TAG}"
+                else
+                    QUERY_LABELS="${SERVICE_LABELS}"
+                fi
+
+                DEPLOYMENT=`kubectl --namespace=${NAMESPACE} get deployments -l \${QUERY_LABELS} -o name | head -n 1`
+
+                # Wait for deployment to be ready
+                kubectl --namespace=${NAMESPACE} rollout status \${DEPLOYMENT}
+
+                # Port forwarding & logs
+                kubectl --namespace=${NAMESPACE} port-forward \${DEPLOYMENT} ${MICROSERVICE_PORT} ${MANAGEMENT_PORT} &
+                kubectl --namespace=${NAMESPACE} logs -f \${DEPLOYMENT} &
+                echo "Sleeping for 3 seconds while connection is established..."
+                sleep 3
+
+                # Let the application start
+                bash scripts/health_check.sh "http://127.0.0.1:${MANAGEMENT_PORT}"
+
+                # Run tests
+                bash scripts/api_tests.sh 127.0.0.1 ${MICROSERVICE_PORT}
+
+                # Kill port forwarding
+                killall kubectl || true
+                """
+            }
+        }
+    }
+}
